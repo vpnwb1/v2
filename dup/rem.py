b9df0e26fa088db78ccf3c6f2b7465298911f662
ametz1313 <126824845+ametz1313@users.noreply.github.com>
2023-10-10 01:48:45 +0330

    Update rem.py

--- dup/rem.py
+++ dup/rem.py
@@ -1,6 +1,6 @@
 import os
 
-files_to_remove = ['tuic.txt', 'hy2.txt', 'Ss.txt', 'Ssr.txt', 'Vmess.txt', 'Vless.txt', 'Trojan.txt', 'Tuic.txt', 'Hy2.txt']
+files_to_remove = ['tuic.txt', 'hy2.txt', 'Ss.txt', 'Ssr.txt', 'Vmess.txt', 'Vless.txt', 'Trojan.txt']#, 'Tuic.txt', 'Hy2.txt']
 
 for file in files_to_remove:
     if os.path.exists(file):
