7e93f794c8160895319ef6df7bf8496036736b50
ametz1313 <126824845+ametz1313@users.noreply.github.com>
2023-10-20 03:19:08 +0330

    1

--- hy2tel/2.py
+++ hy2tel/2.py
@@ -23,7 +23,7 @@ random_urls = random.sample(urls, num_urls_to_send)
 # Format each URL in monospaced type
 formatted_urls = [f'`{url}`' for url in random_urls]
 
-message_text = 'Hysteria2 | هیستریا  \n\n{}\n@UncensorX '.format('\n'.join(formatted_urls))
+message_text = 'Hysteria2 | هیستریا  \n\n{}\n\n@UncensorX '.format('\n\n'.join(formatted_urls))
 
 message_payload = {
     'text': message_text,
