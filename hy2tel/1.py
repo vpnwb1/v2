1949f46f9cb887bb4a2012c5a556ba747958043f
ametz1313 <126824845+ametz1313@users.noreply.github.com>
2023-10-20 03:27:23 +0330

    Update 1.py

--- hy2tel/1.py
+++ hy2tel/1.py
@@ -13,13 +13,15 @@ with open('Hy2L.txt', 'w') as file2:
 
 ###
 
-
 def move_lines_randomly(source_file, destination_file, num_lines):
     lines = []
-    selected_lines = []
 
     with open(source_file, 'r') as source:
-        lines = source.readlines()
+        lines = [line.strip() for line in source if len(line.strip()) >= 8]
+
+    if not lines:
+        print("No lines with at least 8 characters found.")
+        return
 
     random.shuffle(lines)
     selected_lines = lines[:num_lines]
