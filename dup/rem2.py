72f5e2924fb96e122ebd043d6b675c06529d7bf9
ametz1313 <126824845+ametz1313@users.noreply.github.com>
2023-12-06 21:52:08 +0000

    new file:   HysteriaL.txt 	modified:   SsL.txt 	modified:   SsrL.txt 	modified:   TrojanL.txt 	modified:   VlessL.txt 	modified:   VmessL.txt 	modified:   dup/rem2.py

--- dup/rem2.py
+++ dup/rem2.py
@@ -1,6 +1,6 @@
 import os
 
-files_to_remove = ['fromtele/merged.txt', 'fromtele/mergedclean.txt', 'fromtele/juicity.txt', 'fromtele/ss.txt', 'fromtele/ssr.txt', 'fromtele/vmess.txt', 'fromtele/vless.txt', 'fromtele/trojan.txt', 'fromtele/tuic.txt', 'fromtele/hy2.txt', 'fromtele/hysteria.txt', 'merged.txt', 'merged_file.txt', 'hysteria.txt', 'Hysteria.txt' ]
+files_to_remove = ['fromtele/merged.txt', 'fromtele/mergedclean.txt', 'fromtele/juicity.txt', 'fromtele/ss.txt', 'fromtele/ssr.txt', 'fromtele/vmess.txt', 'fromtele/vless.txt', 'fromtele/trojan.txt', 'fromtele/tuic.txt', 'fromtele/hy2.txt', 'fromtele/hysteria.txt', 'merged.txt', 'merged_file.txt', 'hysteria.txt', 'Hysteria.txt', 'hysteriaL.txt' ]
 
 for file in files_to_remove:
     if os.path.exists(file):
