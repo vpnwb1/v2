93db93857aefd5fb6ef227c2c5e797a46ef67476
ametz1313 <126824845+ametz1313@users.noreply.github.com>
2023-12-22 02:33:03 +0330

    Update Send.py

--- Send.py
+++ Send.py
@@ -1,29 +1,29 @@
 import os
 import requests
 import json
-import random
 
 BOT_TOKEN = os.environ['BOT_TOKEN']
 BASE_URL = f'https://api.telegram.org/bot{BOT_TOKEN}/'
 
 CHAT_ID_4 = os.environ['CHAT_ID_4']
 
-file_path = 'SsL.txt'
+file_paths = ['SsL.txt', 'SsrL.txt', 'VmessL.txt']  # List of file paths to send
 
-with open(file_path, 'rb') as file:
-    document_payload = {
-        'chat_id': CHAT_ID_4,
-    }
+for file_path in file_paths:
+    with open(file_path, 'rb') as file:
+        document_payload = {
+            'chat_id': CHAT_ID_4,
+        }
 
-    files = {
-        'document': file,
-    }
+        files = {
+            'document': file,
+        }
 
-    response = requests.post(BASE_URL + 'sendDocument', data=document_payload, files=files)
+        response = requests.post(BASE_URL + 'sendDocument', data=document_payload, files=files)
 
-if response.status_code == 200:
-    print('File sent successfully to the channel!')
-else:
-    print('Failed to send the file to the channel.')
-    print(f'Response status code: {response.status_code}')
-    print(response.text)
+    if response.status_code == 200:
+        print(f'File "{file_path}" sent successfully to the channel!')
+    else:
+        print(f'Failed to send the file "{file_path}" to the channel.')
+        print(f'Response status code: {response.status_code}')
+        print(response.text)
