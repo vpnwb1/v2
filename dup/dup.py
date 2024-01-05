60179dfd8e1249207c583d98864b6f1ad209b4e5
ametz1313 <126824845+ametz1313@users.noreply.github.com>
2024-01-05 19:41:09 +0330

    Update dup.py

--- dup/dup.py
+++ dup/dup.py
@@ -1,19 +1,24 @@
 import hashlib
 
 input_file_path = "Ss.txt"
-output_file_path = "SsL.txt"
+output_file_path_1 = "SsL.txt"
+output_file_path_2 = "SsF.txt"
 
 completed_lines_hash = set()
 
 input_file = open(input_file_path, "r")
-output_file = open(output_file_path, "w")
+output_file_1 = open(output_file_path_1, "w")
+output_file_2 = open(output_file_path_2, "w")
 
 for line in input_file:
-  hashValue = hashlib.md5(line.rstrip().encode('utf-8')).hexdigest()
-  if hashValue not in completed_lines_hash:
-    output_file.write(line)
-    completed_lines_hash.add(hashValue)
-output_file.close()
+    hashValue = hashlib.md5(line.rstrip().encode('utf-8')).hexdigest()
+    if hashValue not in completed_lines_hash:
+        output_file_1.write(line)
+        output_file_2.write(line)
+        completed_lines_hash.add(hashValue)
+
+output_file_1.close()
+output_file_2.close()
 input_file.close()
 
 ####
@@ -21,19 +26,24 @@ input_file.close()
 import hashlib
 
 input_file_path = "Ssr.txt"
-output_file_path = "SsrL.txt"
+output_file_path_1 = "SsrL.txt"
+output_file_path_1_2 = "SsrF.txt"
 
 completed_lines_hash = set()
 
 input_file = open(input_file_path, "r")
-output_file = open(output_file_path, "w")
+output_file_1 = open(output_file_path_1_1, "w")
+output_file_2 = open(output_file_path_1_2, "w")
 
 for line in input_file:
-  hashValue = hashlib.md5(line.rstrip().encode('utf-8')).hexdigest()
-  if hashValue not in completed_lines_hash:
-    output_file.write(line)
-    completed_lines_hash.add(hashValue)
-output_file.close()
+    hashValue = hashlib.md5(line.rstrip().encode('utf-8')).hexdigest()
+    if hashValue not in completed_lines_hash:
+        output_file_1.write(line)
+        output_file_2.write(line)
+        completed_lines_hash.add(hashValue)
+
+output_file_1.close()
+output_file_2.close()
 input_file.close()
 
 ####
@@ -41,19 +51,24 @@ input_file.close()
 import hashlib
 
 input_file_path = "Trojan.txt"
-output_file_path = "TrojanL.txt"
+output_file_path_1 = "TrojanL.txt"
+output_file_path_2 = "TrojanF.txt"
 
 completed_lines_hash = set()
 
 input_file = open(input_file_path, "r")
-output_file = open(output_file_path, "w")
+output_file_1 = open(output_file_path_1, "w")
+output_file_2 = open(output_file_path_2, "w")
 
 for line in input_file:
-  hashValue = hashlib.md5(line.rstrip().encode('utf-8')).hexdigest()
-  if hashValue not in completed_lines_hash:
-    output_file.write(line)
-    completed_lines_hash.add(hashValue)
-output_file.close()
+    hashValue = hashlib.md5(line.rstrip().encode('utf-8')).hexdigest()
+    if hashValue not in completed_lines_hash:
+        output_file_1.write(line)
+        output_file_2.write(line)
+        completed_lines_hash.add(hashValue)
+
+output_file_1.close()
+output_file_2.close()
 input_file.close()
 
 ####
@@ -61,19 +76,24 @@ input_file.close()
 import hashlib
 
 input_file_path = "Vmess.txt"
-output_file_path = "VmessL.txt"
+output_file_path_1 = "VmessL.txt"
+output_file_path_2 = "VmessF.txt"
 
 completed_lines_hash = set()
 
 input_file = open(input_file_path, "r")
-output_file = open(output_file_path, "w")
+output_file_1 = open(output_file_path_1, "w")
+output_file_2 = open(output_file_path_2, "w")
 
 for line in input_file:
-  hashValue = hashlib.md5(line.rstrip().encode('utf-8')).hexdigest()
-  if hashValue not in completed_lines_hash:
-    output_file.write(line)
-    completed_lines_hash.add(hashValue)
-output_file.close()
+    hashValue = hashlib.md5(line.rstrip().encode('utf-8')).hexdigest()
+    if hashValue not in completed_lines_hash:
+        output_file_1.write(line)
+        output_file_2.write(line)
+        completed_lines_hash.add(hashValue)
+
+output_file_1.close()
+output_file_2.close()
 input_file.close()
 
 ####
@@ -81,19 +101,24 @@ input_file.close()
 import hashlib
 
 input_file_path = "Vless.txt"
-output_file_path = "VlessL.txt"
+output_file_path_1 = "VlessL.txt"
+output_file_path_2 = "VlessF.txt"
 
 completed_lines_hash = set()
 
 input_file = open(input_file_path, "r")
-output_file = open(output_file_path, "w")
+output_file_1 = open(output_file_path_1, "w")
+output_file_2 = open(output_file_path_2, "w")
 
 for line in input_file:
-  hashValue = hashlib.md5(line.rstrip().encode('utf-8')).hexdigest()
-  if hashValue not in completed_lines_hash:
-    output_file.write(line)
-    completed_lines_hash.add(hashValue)
-output_file.close()
+    hashValue = hashlib.md5(line.rstrip().encode('utf-8')).hexdigest()
+    if hashValue not in completed_lines_hash:
+        output_file_1.write(line)
+        output_file_2.write(line)
+        completed_lines_hash.add(hashValue)
+
+output_file_1.close()
+output_file_2.close()
 input_file.close()
 
 ####
@@ -108,19 +133,24 @@ input_file.close()
 import hashlib
 
 input_file_path = "Hy2.txt"
-output_file_path = "Hy2L.txt"
+output_file_path_1 = "Hy2L.txt"
+output_file_path_2 = "Hy2F.txt"
 
 completed_lines_hash = set()
 
 input_file = open(input_file_path, "r")
-output_file = open(output_file_path, "w")
+output_file_1 = open(output_file_path_1, "w")
+output_file_2 = open(output_file_path_2, "w")
 
 for line in input_file:
-  hashValue = hashlib.md5(line.rstrip().encode('utf-8')).hexdigest()
-  if hashValue not in completed_lines_hash:
-    output_file.write(line)
-    completed_lines_hash.add(hashValue)
-output_file.close()
+    hashValue = hashlib.md5(line.rstrip().encode('utf-8')).hexdigest()
+    if hashValue not in completed_lines_hash:
+        output_file_1.write(line)
+        output_file_2.write(line)
+        completed_lines_hash.add(hashValue)
+
+output_file_1.close()
+output_file_2.close()
 input_file.close()
 
 ####
@@ -134,25 +164,27 @@ input_file.close()
 import hashlib
 
 input_file_path = "Tuic.txt"
-output_file_path = "TuicL.txt"
+output_file_path_1 = "TuicL.txt"
+output_file_path_2 = "TuicF.txt"
 
 completed_lines_hash = set()
 
 input_file = open(input_file_path, "r")
-output_file = open(output_file_path, "w")
+output_file_1 = open(output_file_path_1, "w")
+output_file_2 = open(output_file_path_2, "w")
 
 for line in input_file:
-  hashValue = hashlib.md5(line.rstrip().encode('utf-8')).hexdigest()
-  if hashValue not in completed_lines_hash:
-    output_file.write(line)
-    completed_lines_hash.add(hashValue)
-output_file.close()
+    hashValue = hashlib.md5(line.rstrip().encode('utf-8')).hexdigest()
+    if hashValue not in completed_lines_hash:
+        output_file_1.write(line)
+        output_file_2.write(line)
+        completed_lines_hash.add(hashValue)
+
+output_file_1.close()
+output_file_2.close()
 input_file.close()
 
 ####
-
-
-
 
 
 
@@ -160,23 +192,27 @@ input_file.close()
 import hashlib
 
 input_file_path = "Hysteria.txt"
-output_file_path = "HysteriaL.txt"
+output_file_path_1 = "HysteriaL.txt"
+output_file_path_2 = "HysteriaF.txt"
 
 completed_lines_hash = set()
 
 input_file = open(input_file_path, "r")
-output_file = open(output_file_path, "w")
+output_file_1 = open(output_file_path_1, "w")
+output_file_2 = open(output_file_path_2, "w")
 
 for line in input_file:
-  hashValue = hashlib.md5(line.rstrip().encode('utf-8')).hexdigest()
-  if hashValue not in completed_lines_hash:
-    output_file.write(line)
-    completed_lines_hash.add(hashValue)
-output_file.close()
+    hashValue = hashlib.md5(line.rstrip().encode('utf-8')).hexdigest()
+    if hashValue not in completed_lines_hash:
+        output_file_1.write(line)
+        output_file_2.write(line)
+        completed_lines_hash.add(hashValue)
+
+output_file_1.close()
+output_file_2.close()
 input_file.close()
 
 ####
-
 
 
 
@@ -186,19 +222,24 @@ input_file.close()
 import hashlib
 
 input_file_path = "juicity.txt"
-output_file_path = "juicityL.txt"
+output_file_path_1 = "juicityL.txt"
+output_file_path_2 = "juicityF.txt"
 
 completed_lines_hash = set()
 
 input_file = open(input_file_path, "r")
-output_file = open(output_file_path, "w")
+output_file_1 = open(output_file_path_1, "w")
+output_file_2 = open(output_file_path_2, "w")
 
 for line in input_file:
-  hashValue = hashlib.md5(line.rstrip().encode('utf-8')).hexdigest()
-  if hashValue not in completed_lines_hash:
-    output_file.write(line)
-    completed_lines_hash.add(hashValue)
-output_file.close()
+    hashValue = hashlib.md5(line.rstrip().encode('utf-8')).hexdigest()
+    if hashValue not in completed_lines_hash:
+        output_file_1.write(line)
+        output_file_2.write(line)
+        completed_lines_hash.add(hashValue)
+
+output_file_1.close()
+output_file_2.close()
 input_file.close()
 
 ####
